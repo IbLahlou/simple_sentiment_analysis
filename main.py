69ab8d4baccb909d0cff912ed42d15a8ea6128ad
ibrahim Lahlou <ibrahim.lahlou021@gmail.com>
2023-07-24 21:43:37 +0100

    Sentiment analysis

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from newspaper import Article
+from textblob import TextBlob
 
-url = 'https://en.wikipedia.org/wiki/Mathematics'
+url = 'https://www.theguardian.com/world/2023/jul/20/auckland-shooting-police-shut-down-cbd-streets-amid-reports-of-gunman'
 article = Article(url)
 
 article.download()
@@ -9,3 +10,8 @@ article.nlp()
 
 text = article.summary
 print(text)
+
+blob = TextBlob(text)
+sentiment =blob.sentiment.polarity # - 1 to 1
+print(sentiment)
+
