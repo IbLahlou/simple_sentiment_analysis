c85dc929552f37a0fabd1e682e0d45f36bb2e3c2
ibrahim Lahlou <ibrahim.lahlou021@gmail.com>
2023-07-24 22:02:54 +0100

    Sentiment analysis -v1

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from newspaper import Article
 from textblob import TextBlob
 
-url = 'https://www.theguardian.com/world/2023/jul/20/auckland-shooting-police-shut-down-cbd-streets-amid-reports-of-gunman'
-article = Article(url)
 
-article.download()
-article.parse()
-article.nlp()
+choice = input("Local Text or Online ? : (Y/N)")
+if (choice == 'Y'):
+    print("Tap the url in the code")
+    url = 'https://www.theguardian.com/world/2023/jul/20/auckland-shooting-police-shut-down-cbd-streets-amid-reports-of-gunman'
+    article = Article(url)
 
-text = article.summary
-print(text)
+    article.download()
+    article.parse()
+    article.nlp()
+
+    text = article.summary
+    print(text)
+
+    
+else :
+    print("Tap the path of Which file you want")
+    with open('negative_file.txt', 'r') as f:
+        text = f.read()
 
 blob = TextBlob(text)
 sentiment =blob.sentiment.polarity # - 1 to 1
